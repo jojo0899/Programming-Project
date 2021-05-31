5119e9a4822670b3fc1238597398a7c138f9d170
MrMcLovin <83535371+AchtFizzle@users.noreply.github.com>
2021-05-31 21:09:42 +0200

    User+Eventklasse

---
+++ BeddelEvent/src/Functionalities/Event.java
@@ -0,0 +1,14 @@
+package Functionalities;
+
+public class Event {
+	public String Sport;
+	public String Date;
+	public String Time;
+	public String Anz;
+	public String Kosten;
+	public String Veranstalter;
+	
+	
+	
+
+}
---
+++ BeddelEvent/src/Functionalities/User.java
@@ -0,0 +1,13 @@
+package Functionalities;
+
+public class User {
+	public static String username;
+	public static String firstName;
+	public static String lastName;
+	public static String gender;
+	public static String passwort;
+	public static boolean bool;
+	public static String email; 
+	
+
+}
